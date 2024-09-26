a4d2d12b41908ef033421bcea3402b7cb21dc45c
Mohit Goyal <mohit.goyal@drdroid.io>
2024-09-26 18:40:55 +0530

    adds docker setup

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -10,5 +10,5 @@ WORKER_MAX_REQUEST_JITTER="${GUNICORN_WORKER_MAX_REQUEST_JITTER:-10000}"
 WORKER_TIMEOUT="${GUNICORN_WORKER_TIMEOUT:-120}"
 
 python manage.py collectstatic --noinput
-gunicorn playbooks.wsgi --bind 0.0.0.0:8000 --workers $WORKER_COUNT --timeout $WORKER_TIMEOUT --max-requests $WORKER_MAX_REQUEST --max-requests-jitter $WORKER_MAX_REQUEST_JITTER &
+gunicorn agent.wsgi --bind 0.0.0.0:8000 --workers $WORKER_COUNT --timeout $WORKER_TIMEOUT --max-requests $WORKER_MAX_REQUEST --max-requests-jitter $WORKER_MAX_REQUEST_JITTER &
 nginx -g "daemon off;"
