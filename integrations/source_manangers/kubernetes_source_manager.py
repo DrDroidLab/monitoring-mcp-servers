8d62e2e4c79091944e93a20c2196d7a9f699ac95
droid-dhruv <dhruv.popuri@drdroid.io>
2025-07-25 18:12:57 +0530

    added write k8s task

--- integrations/source_manangers/kubernetes_source_manager.py
+++ integrations/source_manangers/kubernetes_source_manager.py
@@ -30,11 +30,55 @@ class KubernetesSourceManager(SourceManager):
                               form_field_type=FormFieldType.MULTILINE_FT),
                 ]
             },
+            Kubectl.TaskType.K8S_WRITE_COMMAND: {
+                'executor': self.execute_write_command,
+                'model_types': [],
+                'result_type': PlaybookTaskResultType.BASH_COMMAND_OUTPUT,
+                'display_name': 'Execute a Kubectl Write Command',
+                'category': 'Actions',
+                'form_fields': [
+                    FormField(key_name=StringValue(value="command"),
+                              display_name=StringValue(value="Kubectl Write Command"),
+                              description=StringValue(value='e.g. "kubectl apply -f deployment.yaml", "kubectl scale deployment nginx --replicas=3", "kubectl delete pod my-pod -n default"'),
+                              helper_text=StringValue(value="Enter the kubectl command that will modify the cluster state"),
+                              data_type=LiteralType.STRING,
+                              form_field_type=FormFieldType.MULTILINE_FT),
+                ]
+            },
         }
 
     def get_connector_processor(self, kubernetes_connector, **kwargs):
         generated_credentials = generate_credentials_dict(kubernetes_connector.type, kubernetes_connector.keys)
         return KubectlApiProcessor(**generated_credentials)
+    
+    def execute_write_command(self, time_range: TimeRange, kubernetes_task: Kubectl,
+                    kubernetes_connector: ConnectorProto):
+        try:
+            command_str = kubernetes_task.k8s_write_command.command.value
+            commands = command_str.split('\n')
+
+            try:
+                outputs = {}
+                kubectl_client = self.get_connector_processor(kubernetes_connector)
+                for command in commands:
+                    output = kubectl_client.execute_command(command)
+                    outputs[command] = output
+                command_output_protos = []
+                for command, output in outputs.items():
+                    bash_command_result = BashCommandOutputResult.CommandOutput(command=StringValue(value=command),
+                                                                                output=StringValue(value=output))
+                    command_output_protos.append(bash_command_result)
+                return PlaybookTaskResult(
+                    source=self.source,
+                    type=PlaybookTaskResultType.BASH_COMMAND_OUTPUT,
+                    bash_command_output=BashCommandOutputResult(
+                        command_outputs=command_output_protos
+                    )
+                )
+            except Exception as e:
+                raise Exception(f"Error while executing Kubernetes task: {e}")
+        except Exception as e:
+            raise Exception(f"Error while executing Kubernetes task: {e}")
 
     def execute_command(self, time_range: TimeRange, kubernetes_task: Kubectl,
                         kubernetes_connector: ConnectorProto):
