0802e28974fd93b896fadec5cbc59062877c92ee
droid-dhruv <dhruv.popuri@drdroid.io>
2025-07-31 14:14:31 +0530

    use proto 5.x to accomodate langevals

--- scripts/proto_codegen.sh
+++ scripts/proto_codegen.sh
@@ -5,20 +5,18 @@ set -ex
 
 repo_root="$(git rev-parse --show-toplevel)"
 PROTO_REPO_DIR="$repo_root/protos"
-venv_dir="/tmp/proto_codegen_venv"
-
 
 cd "$repo_root"/
 
 # clean up old generated code
 find "$repo_root"/protos/ -regex ".*_pb2.*\.pyi?" -exec rm {} +
 
-# generate proto code for all protos
+# generate proto code for all protos using existing environments
 all_protos=$(find "$PROTO_REPO_DIR" -iname "*.proto")
-python -m grpc_tools.protoc \
+uv run python3 -m grpc_tools.protoc \
     -I "$repo_root" \
     --python_out=. \
     --mypy_out=. \
     $all_protos
-
+uv pip show protobuf
 echo "Latest proto generation done."
